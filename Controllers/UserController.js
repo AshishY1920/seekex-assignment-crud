6e8b1037928ab5c88885b2f73795982f32d9fde6
AshishY1920 <ashish.yadav@menrocks.in>
2024-01-19 14:22:44 +0530

    added_deleteted_api

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,112 +1,141 @@
-const User = require('../Models/UserModel')
-
+const User = require("../Models/UserModel");
 
 // Create a User
 exports.createUser = async (req, res) => {
-    try {
-        const { name, email, dob, country } = req.body;
-        if (!name || !email || !dob || !country) {
-            return res.status(400).json({
-                success: false,
-                message: "Please fill all the required fields"
-            })
-        }
-
-        let user = await User.create({
-            name, email, dob, avatar: {
-                public_id: "Dummy_Img",
-                url: "https://yadavashishdhirendra.github.io/MLG-Newsletter-1.github.io/profile-boy.png"
-            }, country
-        })
-
-
-        res.status(200).json({
-            success: true,
-            message: "User Created Success",
-            user,
-        })
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        })
+  try {
+    const { name, email, dob, country } = req.body;
+    if (!name || !email || !dob || !country) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all the required fields",
+      });
     }
-}
+
+    let user = await User.create({
+      name,
+      email,
+      dob,
+      avatar: {
+        public_id: "Dummy_Img",
+        url: "https://yadavashishdhirendra.github.io/MLG-Newsletter-1.github.io/profile-boy.png",
+      },
+      country,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User Created Success",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // Get All User
 exports.getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find({}).sort({ _id: -1 })
-        if (users.length <= 0) {
-            return res.status(400).json({
-                success: false,
-                message: "There is No Data to show"
-            })
-        }
-
-        res.status(200).json({
-            success: true,
-            users,
-        })
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        })
+  try {
+    const users = await User.find({}).sort({ _id: -1 });
+    if (users.length <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "There is No Data to show",
+      });
     }
-}
+
+    res.status(200).json({
+      success: true,
+      users,
+      message: "User Retrieved Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // get single user
 exports.getSingleUser = async (req, res) => {
-    try {
-      const users = await User.findById(req.params.id);
-      if (!users) {
-        return res.status(400).json({
-          success: false,
-          message: "There is No Data to show",
-        });
-      }
-  
-      res.status(200).json({
-        success: true,
-        users,
-      });
-    } catch (error) {
-      res.status(500).json({
+  try {
+    const users = await User.findById(req.params.id);
+    if (!users) {
+      return res.status(400).json({
         success: false,
-        message: error.message,
+        message: "There is No Data to show",
       });
     }
-  };
+
+    res.status(200).json({
+      success: true,
+      users,
+      message: "User Retrieved Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // Update Single Users
 exports.updateUser = async (req, res) => {
-    try {
-        const user = {
-            name: req.body.name,
-            email: req.body.email,
-            dob: req.body.dob,
-            country: req.body.country,
-        }
+  try {
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+      dob: req.body.dob,
+      country: req.body.country,
+    };
 
-        const Updateduser = await User.findByIdAndUpdate(req.params.id, user);
-        if (!user) {
-            return res.status(400).json({
-                success: false,
-                message: "There is No Users"
-            })
-        }
-
-        res.status(200).json({
-            success: true,
-            Updateduser
-        })
-
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        })
+    const Updateduser = await User.findByIdAndUpdate(req.params.id, user);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "No Users Found",
+      });
     }
-}
+
+    res.status(200).json({
+      success: true,
+      Updateduser,
+      message: "User Updated Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Delete a User
+exports.deleteUser = async (req, res) => {
+  try {
+    const deleteUser = await User.findByIdAndDelete(req.params.id);
+
+    if (deleteUser === null || deleteUser === undefined || deleteUser === "") {
+      return res.status(400).json({
+        success: false,
+        message: "No User Found",
+      });
+    }
+    console.log(deleteUser);
+
+    return res.status(200).json({
+      success: true,
+      message: "User Deleted Successfully",
+      deleteUser: deleteUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser, getAllUsers, updateUser, getSingleUser } = require('../Controllers/UserController');
+const { createUser, getAllUsers, updateUser, getSingleUser, deleteUser } = require('../Controllers/UserController');
 const router = express.Router()
 
 
@@ -7,5 +7,6 @@ router.route("/create/user").post(createUser);
 router.route("/get/users").get(getAllUsers);
 router.route("/update/user/:id").put(updateUser);
 router.route("/get/single/user/:id").get(getSingleUser);
+router.route("/delete/user/:id").delete(deleteUser);
 
 module.exports = router;
